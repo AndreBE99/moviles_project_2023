93ac70b458e4652524ea5f9ad7d19d0271717b2b
Juan Montero <monteroespina@hotmail.com>
2023-04-09 01:58:43 -0600

    added functionality for barcode and speech pages.

--- product_finder/lib/pages/search_pages/speech_page.dart
+++ product_finder/lib/pages/search_pages/speech_page.dart
@@ -1,7 +1,148 @@
+// import 'package:flutter/material.dart';
+
+// class SpeechPage extends StatelessWidget {
+//   const SpeechPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         leading: IconButton(
+//           icon: Icon(Icons.arrow_back),
+//           onPressed: () {
+//             Navigator.of(context).pop();
+//           },
+//         ),
+//         title: Text('Material App Bar'),
+//       ),
+//       body: Column(
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: [
+//           Stack(
+//             children: [
+//               Container(
+//                 height: 200,
+//                 decoration: BoxDecoration(
+//                   image: DecorationImage(
+//                     image: AssetImage('assets/images/image.png'),
+//                     fit: BoxFit.cover,
+//                   ),
+//                 ),
+//               ),
+//               Container(
+//                 alignment: Alignment.center,
+//                 child: Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     SizedBox(height: 68),
+//                     Text(
+//                       'Flutter Met',
+//                       style: TextStyle(
+//                         color: Colors.white,
+//                         fontSize: 36,
+//                         fontWeight: FontWeight.bold,
+//                       ),
+//                     ),
+//                     SizedBox(height: 16),
+//                     Text(
+//                       'Your place for searching ART',
+//                       style: TextStyle(
+//                         color: Colors.white,
+//                         fontSize: 18,
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ],
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(16),
+//             child: TextField(
+//               decoration: InputDecoration(
+//                 hintText: 'Speech',
+//                 prefixIcon: Icon(Icons.search),
+//                 border: OutlineInputBorder(),
+//               ),
+//             ),
+//           ),
+//           SizedBox(
+//             height: 48,
+//             child: ElevatedButton(
+//               onPressed: () {},
+//               child: Text('Search'),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+
+void main() {
+  runApp(SpeechPage());
+}
 
 class SpeechPage extends StatelessWidget {
-  const SpeechPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  SpeechToText _speechToText = SpeechToText();
+  bool _speechEnabled = false;
+  String _lastWords = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _initSpeech();
+  }
+
+  /// This has to happen only once per app
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    setState(() {});
+  }
+
+  /// Each time to start a speech recognition session
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {});
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  /// This is the callback that the SpeechToText plugin calls when
+  /// the platform returns recognized words.
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      _lastWords = result.recognizedWords;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,26 +156,23 @@ class SpeechPage extends StatelessWidget {
         ),
         title: Text('Material App Bar'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Stack(
-            children: [
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/image.png'),
-                    fit: BoxFit.cover,
-                  ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/image.png'),
+                  fit: BoxFit.cover,
                 ),
               ),
-              Container(
+              child: Container(
                 alignment: Alignment.center,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    SizedBox(height: 68),
                     Text(
                       'Flutter Met',
                       style: TextStyle(
@@ -54,26 +192,45 @@ class SpeechPage extends StatelessWidget {
                   ],
                 ),
               ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Speech',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(),
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                'Press the microphone to hear the word',
+                style: TextStyle(fontSize: 20.0),
               ),
             ),
-          ),
-          SizedBox(
-            height: 48,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text('Search'),
+            ElevatedButton(
+              onPressed: _speechToText.isNotListening
+                  ? _startListening
+                  : _stopListening,
+              child: Icon(
+                  _speechToText.isNotListening ? Icons.mic_off : Icons.mic),
             ),
-          ),
-        ],
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  // If listening is active show the recognized words
+                  _speechToText.isListening
+                      ? '$_lastWords'
+                      // If listening isn't active but could be tell the user
+                      // how to start it, otherwise indicate that speech
+                      // recognition is not yet ready or not supported on
+                      // the target device
+                      : _speechEnabled
+                          ? 'Tap the microphone to start listening...'
+                          : 'Speech not available',
+                ),
+              ),
+            ),
+            Text(
+              'Scan results : $_lastWords \n',
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            )
+          ],
+        ),
       ),
     );
   }
