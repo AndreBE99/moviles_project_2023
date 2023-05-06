cde1228febcac8a0ce6c28112080a1ed94eb34bf
AndreBE99 <andrebetan.iteso@gmail.com>
2023-05-06 00:28:07 -0600

    Merge auth and styles

--- product_finder/lib/main.dart
+++ product_finder/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:product_finder/auth/bloc/auth_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:product_finder/auth/bloc/auth_bloc.dart';
 import 'package:product_finder/pages/favorites/bloc/favorites_bloc.dart';
 import 'package:product_finder/pages/home_page.dart';
 import 'package:product_finder/pages/login_page.dart';
