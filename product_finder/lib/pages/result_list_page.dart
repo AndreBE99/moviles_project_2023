569a5d6f16d1e6bcf19e490780ebad702a8dbc25
AndreBE99 <andrebetan.iteso@gmail.com>
2023-05-11 22:12:31 -0600

    Firts test

--- product_finder/lib/pages/result_list_page.dart
+++ product_finder/lib/pages/result_list_page.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterfire_ui/firestore.dart';
 import 'package:product_finder/models/producto.dart';
 import 'package:product_finder/pages/product/product_page.dart';
 
@@ -34,10 +32,38 @@ class ResultList extends StatelessWidget {
         title: Text('Product List'),
         centerTitle: true,
       ),
-      body: FirestoreListView(
-        query: FirebaseFirestore.instance.collection("products"),
-        itemBuilder: (context, snapshot) {
-          return Text("${snapshot["name"]}");
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return Container(
+            //margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey, width: 1),
+              borderRadius: BorderRadius.circular(4.0),
+            ),
+            child: ListTile(
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(
+                  product.imageUrl,
+                  fit: BoxFit.contain,
+                  width: 50,
+                  height: 50,
+                ),
+              ),
+              title: Text(product.name),
+              subtitle: Text('Precio: \$${product.price}'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProductPage(product: product),
+                  ),
+                );
+              },
+            ),
+          );
         },
       ),
     );
